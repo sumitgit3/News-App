e9a0714456c472207af4d815395841b8597f012c
sumitgit3 <sumitkumar703011@gmail.com>
2024-06-14 00:37:14 +0530

    populated news Component from array

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
+  
   render() {
+    let {title,desc,imageUrl,url} = this.props;
     return (
       <>
-        <div className="card" style={{width : "18rem"}}>
-        <img src="..." className="card-img-top" alt="..." />
+        <div className="card my-2" style={{width : "18rem"}}>
+        <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="/" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
+            <a href={url} className="btn btn-sm btn-primary">Read more...</a>
           </div>
       </div>
       </>
